ce1f7591838e30a1cb9bd029671ef6a9625f1565
Ulaaka <133600932+Ulaaka@users.noreply.github.com>
2026-03-11 15:00:17 +0000

    now it can send another random digits when the coundown reaches 0, making it more secure

--- UI_main.py
+++ UI_main.py
@@ -1,18 +1,14 @@
-import sys
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import *
-import mysql.connector
+from PyQt5 import QtCore
 from database_connection import database
 from BASE_Classes import password_class
 from qtwidgets import PasswordEdit
 from system_functions import system_functions
-import os
-import certifi
-import sys
+import os, certifi, sys
 from system_functions import system_functions
 from functools import partial
-
 
 system = system_functions()
 # Add the project root to Python path
@@ -40,7 +36,6 @@ input_style = '''
         border: 1px solid white;
     }
     '''
-    
 forgot_button_style = '''
     QPushButton {
         background-color: transparent;
@@ -87,14 +82,21 @@ def handle_button_style(button_color, hover_color):
         '''
         return line
 
+DURATION_INT = 60
+
+def secs_to_minsec(secs: int):
+    mins = secs // 60
+    secs = secs % 60
+    minsec = f'{mins:02}:{secs:02}'
+    return minsec
+
 class login_page(QWidget):
 
-    def __init__(self, controller):
+    def __init__(self, controller, db, cursor):
         super().__init__()
         self.controller = controller
-        connection = database()
-        self.db = connection.db
-        self.cursor = connection.cursor
+        self.db = db
+        self.cursor = cursor
         self.system = system_functions()
 
         self.user_interface()
@@ -168,9 +170,7 @@ class login_page(QWidget):
         layout.addWidget(forgot_password, alignment=Qt.AlignCenter)
         layout.addWidget(signup_btn)
         layout.addStretch()
-
         self.setLayout(layout)
-
 
     def handle_sign_up(self):
         self.controller.show_sign_up()
@@ -206,33 +206,27 @@ class login_page(QWidget):
                 'Please enter your username first, then click "Forgot your password?"'
             )
             return
-
-        email_query = "SELECT email_address FROM users WHERE username = %s"
-        self.cursor.execute(email_query, (username_local, ))
-        email_user = self.cursor.fetchone()[0]
-
-        self.random_digits = self.system.send_reset_digits(6, email_user)
-        print(self.random_digits)
-
+        
+        self.random_digits = self.system.send_reset_digits(6, username_local)
         self.controller.show_reset_form()
         """        QMessageBox.information(
             self, 
             'Password Reset', 
             f'A password reset link has been sent to the email associated with username: {username_local}'
         )"""
-    def get_random_digits(self):
-        return self.random_digits
-    
-    def get_username(self):
-        return self.username.text()
 
 class reset_from_page(QWidget):
-    def __init__(self, controller, login_page):
+    def __init__(self, controller, login_page,  db, cursor):
         super().__init__()
+
+        self.time_left_int = DURATION_INT
+        self.myTimer = QtCore.QTimer(self)
+        self.startTimer()
+        self.system = system_functions()
         self.controller = controller
-        connection = database()
-        self.db = connection.db
-        self.cursor = connection.cursor
+
+        self.db = db
+        self.cursor = cursor
         self.login_page = login_page
         self.user_interface()
 
@@ -260,7 +254,6 @@ class reset_from_page(QWidget):
         title_color.setColor(QPalette.WindowText, QColor("#1877F2"))
         title.setPalette(title_color)
 
-
         # Login button
         submit_btn = QPushButton('Submit')
         submit_btn.setStyleSheet(handle_button_style("#1877F2", "#18d5F2"))
@@ -268,6 +261,11 @@ class reset_from_page(QWidget):
         submit_btn.setFixedHeight(50)
         submit_btn.setCursor(Qt.PointingHandCursor)
         submit_btn.clicked.connect(self.handle_reset_password)
+
+        self.timerLabel = QLabel("01:00", self)
+        self.timerLabel.move(50, 50)
+        self.timerLabel.setAlignment(Qt.AlignCenter)
+        self.timerLabel.setStyleSheet("font: 17pt Helvetica;")
 
         self.squares = []
         centering = Qt.AlignCenter
@@ -289,13 +287,13 @@ class reset_from_page(QWidget):
         layout.addLayout(square_layout)
         self.setLayout(layout)
         layout.addWidget(submit_btn)
+        layout.addWidget(self.timerLabel)
         layout.addStretch()
 
     # needs to be changed
     def to_next_box(self, idx, _):
         if idx < 5:
             self.squares[idx + 1].setFocus()
-
 
     def to_prev_box(self, idx, input):
         if input.key() == Qt.Key_Backspace:
@@ -307,24 +305,43 @@ class reset_from_page(QWidget):
 
     def handle_reset_password(self):
         entered = "".join(i.text() for i in self.squares)
-        if (self.login_page.get_random_digits() == entered):
+        if (self.login_page.random_digits == entered):
             self.controller.show_reset_password()
         else:
             QMessageBox.information(
-                self, 
-                'Code does not match', 
+                self,
+                'Code does not match',
                 'Please try again"'
             )
             return
 
+    # needs to be changed
+    def startTimer(self):
+        self.time_left_int = DURATION_INT
+        self.myTimer.timeout.connect(self.timerTimeout)
+        self.myTimer.start(1000)
+
+    def timerTimeout(self):
+        self.time_left_int -= 1
+
+        if self.time_left_int == 0:
+            self.time_left_int = DURATION_INT
+            self.login_page.random_digits = self.system.send_reset_digits(6, self.login_page.username.text())
+            for i in range(6):
+                self.squares[i].clear()
+        self.update_gui()
+
+    def update_gui(self):
+        minsec = secs_to_minsec(self.time_left_int)
+        self.timerLabel.setText(minsec)
+
 class reset_password(QWidget):
 
-    def __init__(self, controller, login_page):
+    def __init__(self, controller, login_page, db, cursor):
         super().__init__()
         self.controller = controller
-        connection = database()
-        self.db = connection.db
-        self.cursor = connection.cursor
+        self.db = db
+        self.cursor = cursor
         self.login_page = login_page
 
         self.user_interface()
@@ -390,7 +407,7 @@ class reset_password(QWidget):
            print("New Password Matches")
 
            hashed_password = password_manager.hash_password(self.password_1.text())
-           username = self.login_page.get_username()
+           username = self.login_page.username.text()
 
            query = "UPDATE users SET hashed_password = %s WHERE username = %s"
            self.cursor.execute(query, (hashed_password, username))
@@ -409,12 +426,11 @@ class sign_up_page(QWidget):
 
     # https://doc.qt.io/qt-6/stylesheet-reference.html
 
-    def __init__(self, controller):
+    def __init__(self, controller, db, cursor):
         super().__init__()
         self.controller = controller
-        connection = database()
-        self.db = connection.db
-        self.cursor = connection.cursor
+        self.db = db
+        self.cursor = cursor
 
         self.user_interface()
         
@@ -519,18 +535,23 @@ class sign_up_page(QWidget):
 class MainApp(QMainWindow):
     def __init__(self):
         super().__init__()
+
+        connection = database()
+        self.db = connection.db
+        self.cursor = connection.cursor
+
         self.setWindowTitle("Finance Reporter")
         self.setFixedSize(400, 500)
 
         self.stacked_widget = QStackedWidget()
         self.setCentralWidget(self.stacked_widget)
 
-        self.login_page = login_page(self) 
-        self.sign_up_page = sign_up_page(self)
+        self.login_page = login_page(self, self.db, self.cursor) 
+        self.sign_up_page = sign_up_page(self, self.db, self.cursor)
 
         #  # adding reset form page
-        self.reset_form = reset_from_page(self, self.login_page)
-        self.reset_password = reset_password(self, self.login_page)
+        self.reset_form = reset_from_page(self, self.login_page, self.db, self.cursor)
+        self.reset_password = reset_password(self, self.login_page, self.db, self.cursor)
         self.dashboard_page = QWidget()
 
         self.setup_dashboard()
